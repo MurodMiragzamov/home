04fc93f444419dff223adf1c40c9e91a3f45e632
Hugo <blackfoxmmm@gmail.com>
2021-07-04 22:20:33 +0500

    Gotovo

---
+++ app.js
@@ -0,0 +1,17 @@
+let hello = alert('Добро пожаловать)');
+let name = prompt('Ваше имя?');
+let age = prompt('Солько вам лет?');
+let year = prompt('Какого вы года рождения?');
+let adres = prompt('Ваш адрес?');
+let school = prompt('В какой школе вы учитесь?');
+let tel = prompt('Номер телефона?');
+
+alert(
+    "Ваше имя "+ name +
+     "\nВам "+ age + " лет"+
+    "\nВы "+ year +" года рождения"+
+    "\nВаш адрес "+ adres+
+    "\nВы учитесь в школе № "+ school+ 
+    "\nВаш номер телефона "+tel
+
+)
